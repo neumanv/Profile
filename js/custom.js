95ddf968191ff70b4005396dd803cb26d7fa677c
neumanv <guillermoanv25@gmail.com>
2022-11-25 00:40:49 +0100

    Cambios

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,25 @@
 "use strict"
+
+//------------- PANTALLA COMPLETA ---------------------------------------------
+
+//activa la pantalla completa
+function pantallaCompleta(element){
+
+    if(element.webkitRequestFullScreen){
+        element.webkitRequestFullScreen();
+    }
+}
+
+//quita la pantalla completa
+function pantallaNormal(){
+
+    if(document.cancelFullScreen){
+        document.cancelFullScreen();
+
+    }else if(document.mozCancelFullScreen){
+        document.mozCancelFullScreen();
+
+    }else if(document.webkitCancelFullScreen){
+        document.webkitCancelFullScreen();
+    }
+}
