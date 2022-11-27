d0de108aa11416e162aa82e650a18cbbb1e6b01a
neumanv <guillermoanv25@gmail.com>
2022-11-27 13:17:11 +0100

    Cambios

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,34 @@
 //------------- PANTALLA COMPLETA ---------------------------------------------
 
 //activa la pantalla completa (se desactiva con la tecla ESC)
-function pantallaCompleta(element){
+function pantallaCompleta(element) {
 
-    if(element.webkitRequestFullScreen){
+    if (element.webkitRequestFullScreen) {
         element.webkitRequestFullScreen();
     }
 }
+
+//------------- VALIDACIÓN FORMULARIO ------------------------------------------
+
+function validacion() {
+
+    let nombre = document.getElementById("nombre");
+    let tlf = document.getElementById("tlf");
+    let mail = document.getElementById("mail");
+
+    //el .length < 2 es por si es algún usuario con nombre extranjero
+    if (nombre.length < 1 || nombre.length > 20) {
+        alert("Debe de haber entre 2 y 19 caracteres en el nombre.");
+    }
+
+    if(tlf.length != 9){
+        alert("Teléfono inválido");
+    }
+
+    /*for(let i = 0; i > tlf.length; i++){
+        if(tlf[0] != "9" || tlf[0] != "6" || tlf[0] != "7"){
+            alert("El teléfono no existe.");
+        }
+    }*/
+}
+
